7194d40defe8fecbd3fba43fa761e924682d3f81
SaboTak <guideon.slayer@gmail.com>
2023-06-11 21:07:59 -0500

    fix: init process calendar

--- app/components/Navbar/navbar.jsx
+++ app/components/Navbar/navbar.jsx
@@ -1,16 +1,7 @@
 export default function NavbarComponent(){
     return (
         <div className='flex p-5 bg-main'>
-        <div className='flex'>
-            <p className='my-auto px-4 font-semibold text-xl text-white'>Mi Restaurante</p>
-        </div>
-        <div className='ml-auto flex'>
-            <div className='cursor-pointer my-auto flex border-x border-white px-6'>
-                <button className='px-4 py-1 text-white bg-main-opacity ml-4 rounded-2xl font-semibold text-base flex '><p className='mr-10'>Eddy C.</p> <div className=' fechaAbajoBlanca my-auto'></div></button>
-            </div>
-            <div className=' my-auto ml-6'>
-            </div>
-        </div>
+        hola
 
     </div>
     )
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React, { useEffect } from 'react';
+
+import Head from 'next/head';
+import * as ChartJS from 'chart.js';
+
+export default function DashboardPage() {
+
+    return (
+        <div>
+            Hola
+        </div>
+    )
+}
+
